7652c1d066741cedf63206913f9dab949357de20
Time Ranger <varszegi.adam75@gmail.com>
2023-01-31 11:45:14 +0100

    Kiiratás működik

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface State{
+  Csavarok:Csavar[];
+  regTipus:string;
+  regHossz:number;
+  regKeszlet:number;
+  regAr:number;
+}
+
+interface Csavar{
+  id:number;
+  tipus:string;
+  hossz:number;
+  keszlet:number;
+  ar:number;
+}
+
+
+// interface CsavarResponse{
+//   Csavarok:Csavar[];
+// }
+
+class App extends Component<{}, State>{
+  constructor(props:{}){
+    super(props)
+
+    this.state={
+      Csavarok:[],
+      regTipus:'',
+      regHossz:0,
+      regKeszlet:0, 
+      regAr:0.00
+    }
+  }
+
+  async loadCsavarok(){
+    let response=await fetch('http://localhost:3000/csavar')
+    let data=await response.json();
+    this.setState({
+      Csavarok:data
+      
+    });
+  }
+
+  componentDidMount(){
+      this.loadCsavarok();
+     
+  }
+
+
+  render(){
+    const{regTipus, regHossz, regAr, regKeszlet}=this.state;
+      return <div>
+        <div className='container'>
+        <div className='row'>
+        {
+          this.state.Csavarok.map(Csavar=><div className='col-12 col-sm-4'><h2>{Csavar.tipus}<ul><li>Hossz: {Csavar.hossz}mm</li> <li>Készleten: {Csavar.keszlet}</li> <li>Ár: {Csavar.ar}Ft/db</li></ul></h2></div> )
+        }
+      </div>
+      </div>
+
+      </div>
+  }
+
 }
 
 export default App;
